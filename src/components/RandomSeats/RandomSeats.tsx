7f53103514dc09c9d44111fb0a44f9495976e57a
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-10-05 12:19:46 +0900

    [FIX&ADD] RandomSeats style
    ADD-이미지 추가
    FIX-스타일링 수정중

--- src/components/RandomSeats/RandomSeats.tsx
+++ src/components/RandomSeats/RandomSeats.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import RANDOMSEATS_IMG from 'assets/images/RandomSeats/RANDOM_SEAT.png';
 
 import classNames from 'classnames';
 import { ClassNamesFn } from 'classnames/types';
@@ -7,10 +8,23 @@ import Button from 'components/Common/Button/Button';
 const style = require('./RandomSeats.scss');
 const cx: ClassNamesFn = classNames.bind(style);
 
+const buttonCustomStyle = {
+  width: '6rem',
+  height: '2rem',
+  margin: '5px',
+  fontSize: '18px',
+};
+
 const RandomSeats = () => {
   return (
     <>
-      <div className={cx('RandomSeats__left')}></div>
+      <div className={cx('RandomSeats__left')}>
+        <div className={cx('RandomSeats__left-top')}>줄(행)의 수</div>
+        <div className={cx('RandomSeats__left-bottom')}>
+          <img src={RANDOMSEATS_IMG} alt="자리 뽑기" />
+          <Button children="자리 생성" customStyle={buttonCustomStyle} />
+        </div>
+      </div>
       <div className={cx('RandomSeats__right')}></div>
     </>
   );
